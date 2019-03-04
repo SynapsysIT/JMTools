393fd05708473a343d30ba3149a5a437b7760c16
Julien Mazoyer <xephren@gmail.com>
2019-03-04 20:55:43 +0100

    Module Manifest

--- JMTools.psm1
+++ JMTools.psm1
@@ -1,3 +1,2 @@
 # Include functions which are outsourced in .ps1-files
-$WorkSpaceFld = "$env:USERPROFILE\Workspace\WorkInProgress"
 Get-ChildItem -Path "$PSScriptRoot\public" -Recurse | Where-Object {$_.Name.EndsWith(".ps1")} | ForEach-Object {. $_.FullName}
